77bddd6df69491f927bb15b86eabb8e2d51314bc
Andrea Rovescalli <andrea.rovescalli@epfl.ch>
2026-04-17 14:33:42 +0200

    Align project description columns

--- src/js/components/project/ProjectIntro.js
+++ src/js/components/project/ProjectIntro.js
@@ -4,13 +4,13 @@ const ProjectIntro = ({ title, left, right }) => (
   <div className="colsContainer container-fluid remove-paddings-margins">
     <div className="row justify-content-center remove-paddings-margins">
       <div className="col-md-12">
-        <div className="row remove-paddings-margins">
-          <div className="col-md-6">
+        <div className="row remove-paddings-margins projectDescriptionRow">
+          <div className="col-md-6 projectDescriptionColumn">
             <p className="titleContainer">{title}</p>
             {left}
           </div>
-          <div className="col-md-6">
-            <p>&nbsp;</p>
+          <div className="col-md-6 projectDescriptionColumn">
+            <p className="titleContainer projectDescriptionTitleSpacer" aria-hidden="true">{title}</p>
             {right}
           </div>
         </div>
