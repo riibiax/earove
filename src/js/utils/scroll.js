53e4148edcf75b02ca3b7ee322c650a14a627dd2
Andrea Rovescalli <andrea.rovescalli@epfl.ch>
2026-04-18 13:38:32 +0200

    Allow router nav section links

--- src/js/utils/scroll.js
+++ src/js/utils/scroll.js
@@ -3,10 +3,16 @@
 export function initializeScroll() {
   $(document).ready(function () {
     $('#menu-bar a').on('click', function (event) {
+      const href = $(this).attr('href');
+
+      if (!href || href.startsWith('#/')) {
+        return;
+      }
+
       // Prevent default anchor click behavior
       event.preventDefault();
       // Get the target section based on the href attribute of the clicked anchor
-      let target = $($(this).attr('href'));
+      let target = $(href);
       // Check if the target section exists
       if (target.length) {
         // Calculate the offset to scroll to, considering the height of the fixed menu
