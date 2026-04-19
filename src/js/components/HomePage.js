cd90557a165fda5c6f7a08473fba402df3724b15
Andrea Rovescalli <andrea.rovescalli@epfl.ch>
2026-04-19 17:14:14 +0200

    Restore desktop contact layout

--- src/js/components/HomePage.js
+++ src/js/components/HomePage.js
@@ -44,7 +44,7 @@ class HomePage extends Component {
         </section>
 
         <About /> 
-        <Contact className="homeContact d-flex justify-content-center align-items-end" /> 
+        <Contact className="homeContact" /> 
       </>
     );
   }
